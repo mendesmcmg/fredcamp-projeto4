01b64aefde5d81414132b8afc9456006c1f07127
Clara Mendes <claraplanejamento@gmail.com>
2021-07-21 10:37:36 -0300

    :lipstick: adds bar

--- src/components/ItemsPage.js
+++ src/components/ItemsPage.js
@@ -1,9 +1,16 @@
+import { AppBar, Toolbar } from "@material-ui/core";
 import ItemsForm from "./ItemsForm";
 import ItemsList from "./ItemsList";
 
 function ItemsPage() {
   return (
     <>
+      <AppBar position="static">
+        <Toolbar>
+          <h1>Post List</h1>
+        </Toolbar>
+      </AppBar>
+      <br/><br/>
       <ItemsForm />
       <ItemsList />
     </>
