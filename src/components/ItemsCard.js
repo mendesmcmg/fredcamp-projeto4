f2e97974081e2bf68a502246c699feca14a75fdf
Clara Mendes <claraplanejamento@gmail.com>
2021-07-21 10:15:32 -0300

    :recycle: refactors list into items card

--- src/components/ItemsCard.js
+++ src/components/ItemsCard.js
@@ -1,9 +1,16 @@
-const ItemsCard = ({key, title, text, date}) => (
-  <div key={key}>
-    <p>{date}</p>
-    <h1>{title}</h1>
-    <p>{text}</p>
-    <button onClick={console.log("porra")}>Delete</button>
-  </div>
-);
+function deleteItem(){
+    console.log("ui")
+}
+
+
+function ItemsCard({ key, title, text, date }) {
+    return(
+        <div key={key}>
+          <p>{date}</p>
+          <h1>{title}</h1>
+          <p>{text}</p>
+          <button onClick={deleteItem}>Delete</button>
+        </div>
+    )
+}
 export default ItemsCard;
