e0007fc931ff5363d70e2f098925a763c7698b78
Clara Mendes <claraplanejamento@gmail.com>
2021-07-22 14:28:23 -0300

    :sparkles: adds form validation

--- src/components/ItemsForm.js
+++ src/components/ItemsForm.js
@@ -9,6 +9,8 @@ class ItemsForm extends Component {
       posts: [],
       title: "",
       text: "",
+      titleValid: true,
+      textValid: true,
     };
 
     this.handleTitleChange = this.handleTitleChange.bind(this);
@@ -28,43 +30,104 @@ class ItemsForm extends Component {
     });
   }
 
+  checkValid() {
+    if (!this.state.title || this.state.title === "") {
+      this.setState({ titleValid: false });
+      return false;
+    } else {
+      this.setState({ titleValid: true });
+      if (!this.state.text || this.state.text === "") {
+        this.setState({ textValid: false });
+        return false;
+      } else {
+        this.setState({ textValid: true });
+        return true;
+      }
+    }
+  }
+
   createPost(e) {
     e.preventDefault();
     const { title, text } = this.state;
-    const date = new Date().toLocaleString('pt-br');
+    const date = new Date().toLocaleString("pt-br");
 
-    db.ref(`all_posts/`)
-      .push({
-        title,
-        text,
-        date,
-      })
-      .then((_) => {
-        this.setState({ title: "", text: "" });
-      });
+    this.checkValid();
+
+    if (this.checkValid() === true) {
+      db.ref(`all_posts/`)
+        .push({
+          title,
+          text,
+          date,
+        })
+        .then((_) => {
+          this.setState({ title: "", text: "" });
+        });
+    }
   }
 
   render() {
+    let titleField;
+    let textField;
+    if (this.state.titleValid) {
+      titleField = (
+        <TextField
+          fullWidth
+          margin="2rem"
+          label="Title"
+          value={this.state.title}
+          onChange={this.handleTitleChange}
+        />
+      );
+    } else {
+      titleField = (
+        <TextField
+          error
+          helperText="Title required"
+          fullWidth
+          margin="2rem"
+          label="Title"
+          value={this.state.title}
+          onChange={this.handleTitleChange}
+        />
+      );
+    }
+
+    if (this.state.textValid) {
+      textField = (
+        <TextField
+          fullWidth
+          label="Text"
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+      );
+    } else {
+      textField = (
+        <TextField
+          error
+          helperText="Text required"
+          fullWidth
+          label="Text"
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+      );
+    }
     return (
       <Container maxWidth="sm">
         <form onSubmit={this.createPost}>
-          <TextField
-            fullWidth
-            margin="2rem"
-            label="Title"
-            value={this.state.title}
-            onChange={this.handleTitleChange}
-          />
+          {titleField}
           <br />
-          <TextField
-            fullWidth
-            label="Text"
-            value={this.state.text}
-            onChange={this.handleTextChange}
-          />
+          {textField}
           <br />
           <br />
-          <Button variant="contained" color="primary" onClick={this.createPost} type="submit">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.createPost}
+            type="submit"
+          >
             Create Post
           </Button>
         </form>
