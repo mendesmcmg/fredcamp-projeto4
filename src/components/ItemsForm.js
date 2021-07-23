df757bbdd621ef1431b134005600aab83e8ea5ec
fredericodib <fredericopdib@gmail.comclear>
2021-07-22 21:57:38 -0300

    :recycle: Refactor conditional rendering

--- src/components/ItemsForm.js
+++ src/components/ItemsForm.js
@@ -4,7 +4,6 @@ import db from "../utils/firebase";
 
 class ItemsForm extends Component {
   state = {
-    posts: [],
     title: "",
     text: "",
     titleValid: true,
@@ -58,61 +57,29 @@ class ItemsForm extends Component {
   };
 
   render() {
-    let titleField;
-    let textField;
-    if (this.state.titleValid) {
-      titleField = (
-        <TextField
-          fullWidth
-          style={{ marginBottom: 8 }}
-          label="Title"
-          value={this.state.title}
-          onChange={this.handleTitleChange}
-        />
-      );
-    } else {
-      titleField = (
-        <TextField
-          error
-          helperText="Title required"
-          fullWidth
-          style={{ marginBottom: 8 }}
-          label="Title"
-          value={this.state.title}
-          onChange={this.handleTitleChange}
-        />
-      );
-    }
+    const { title, text, titleValid, textValid } = this.state;
 
-    if (this.state.textValid) {
-      textField = (
-        <TextField
-          fullWidth
-          label="Text"
-          value={this.state.text}
-          onChange={this.handleTextChange}
-        />
-      );
-    } else {
-      textField = (
-        <TextField
-          error
-          helperText="Text required"
-          fullWidth
-          label="Text"
-          value={this.state.text}
-          onChange={this.handleTextChange}
-        />
-      );
-    }
     return (
       <Container maxWidth="sm">
         <form onSubmit={this.createPost}>
-          {titleField}
-          <br />
-          {textField}
-          <br />
-          <br />
+          <TextField
+            error={!titleValid}
+            helperText={!titleValid && "Title required"}
+            fullWidth
+            style={{ marginBottom: 8 }}
+            label="Title"
+            value={title}
+            onChange={this.handleTitleChange}
+          />
+          <TextField
+            error={!textValid}
+            helperText={!textValid && "Text required"}
+            fullWidth
+            label="Text"
+            style={{ marginBottom: 8 }}
+            value={text}
+            onChange={this.handleTextChange}
+          />
           <Button
             variant="contained"
             color="primary"
